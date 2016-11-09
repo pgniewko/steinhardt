c5d0cb3cb7aadbb47973f7387bca2f8374cc26dd
Pawel Gniewek <pawel.gniewek@berkeley.edu>
2016-11-09 15:59:16 -0800

    bracketing compile command for safety reasons

--- build_lib.sh
+++ build_lib.sh
@@ -1,11 +1,14 @@
 #! /bin/bash -x
 
 cd ./src/
-rm *.o
-rm libsteinhardt.a 
 
-g++-4.9 -O3 -Wall -lm -lgsl -lgslcblas -c -o steinhardt.o steinhardt.c
-ar -rcs libsteinhardt.a steinhardt.o
-ranlib libsteinhardt.a
-sudo cp libsteinhardt.a /usr/local/lib
-sudo cp steinhardt.h /usr/local/include
+if [ $? ]; then
+  rm *.o
+  rm libsteinhardt.a 
+
+  g++-4.9 -O3 -Wall -lm -lgsl -lgslcblas -c -o steinhardt.o steinhardt.c
+  ar -rcs libsteinhardt.a steinhardt.o
+  ranlib libsteinhardt.a
+  sudo cp libsteinhardt.a /usr/local/lib
+  sudo cp steinhardt.h /usr/local/include
+fi
